8e1cd0a9f2b094b61f3c490c957e07aeb0123a0e
jajoyce <jjoyce4@gmail.com>
2022-01-23 06:07:15 -0500

    Render new.ejs page with form to add new state. Install and set ejs. Add public/styles/main.css. Bulma too.

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const app = express();
 
 const PORT = 4000;
 
+app.set('view engine', 'ejs');
+
+app.use('/static', express.static('public'));
+
 app.get('/', (req, res) => {
     res.send('Yes, this is working.');
+});
+
+app.get('/new', (req, res) => {
+    res.render('new.ejs');
 });
 
 app.get('/*', (req, res) => {
