9cec5291ec731367f2253bb320e4a36a910b46b9
jajoyce <jjoyce4@gmail.com>
2022-01-23 08:08:06 -0500

    Create State schema and model. Add seed data.

--- controllers/state_controller.js
+++ controllers/state_controller.js
@@ -1,6 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const { states } = require('../models');
+const { State } = require('../models');
+
+State.deleteMany({}, (error, deletedState) => {
+    if (error) console.log(error);
+    State.insertMany(
+        [
+            {
+                name: 'Massachusetts', 
+                population: '7 million',
+                demonym: 'Bay Stater', 
+                capital: 'Boston', 
+                largestCity: 'Boston', 
+                flagImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Massachusetts.svg/250px-Flag_of_Massachusetts.svg.png',
+            },
+            {
+                name: 'California', 
+                population: '39.5 million',
+                demonym: 'Californian', 
+                capital: 'Sacramento', 
+                largestCity: 'Los Angeles', 
+                flagImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Flag_of_California.svg/250px-Flag_of_California.svg.png',
+            },
+            {
+                name: 'Texas', 
+                population: '29.1 million',
+                demonym: 'Texan', 
+                capital: 'Austin', 
+                largestCity: 'Houston', 
+                flagImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Flag_of_Texas.svg/250px-Flag_of_Texas.svg.png',
+            },
+        ]
+    )
+});
 
 router.get('/', (req, res) => {
     res.send('Yes, this is working.');
--- models/state_model.js
+++ models/state_model.js
@@ -3,8 +3,25 @@ const mongoose = require('mongoose');
 const stateSchema = new mongoose.Schema({
     name: {
         type: String, 
-        required: true
-    }
+        required: [true, 'State name is required.']
+    }, 
+    population: {
+        type: String, 
+        required: [true, 'Provide the state population as a string, e.g. "10 million"']
+    }, 
+    demonym: {
+        type: String, 
+    }, 
+    capital: {
+        type: String, 
+        required: [true, 'State capital is required.']
+    }, 
+    largestCity: {
+        type: String, 
+    }, 
+    flagImg: {
+        type: String, 
+    }, 
 });
 
 const State = mongoose.model('State', stateSchema);
