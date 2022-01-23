4f257fa6e6ddb6e647b290a50fd4da349b5aa5ca
jajoyce <jjoyce4@gmail.com>
2022-01-23 06:24:31 -0500

    Add 'Create' route to post new state from form. Refactor routes.

--- server.js
+++ server.js
@@ -6,17 +6,24 @@ const PORT = 4000;
 app.set('view engine', 'ejs');
 
 app.use('/static', express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
+
+app.get('/states', (req, res) => {
     res.send('Yes, this is working.');
 });
 
-app.get('/new', (req, res) => {
+app.post('/states', (req, res) => {
+    console.log('Create route accessed');
+    res.send(`Created state: ${req.body.name}`);
+});
+
+app.get('/states/new', (req, res) => {
     res.render('new.ejs');
 });
 
 app.get('/*', (req, res) => {
-    res.redirect('/');
+    res.redirect('/states');
 });
 
 app.listen(PORT, () => console.log(`Running on Port ${PORT}`));
